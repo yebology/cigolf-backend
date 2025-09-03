5d2e39a93cd32bec7904d5a90fd30b7fb78d0353
yebology <139052533+yebology@users.noreply.github.com>
2025-09-03 15:48:41 +0700

    done bug fixing on create weekly plan

--- src/modules/weekly-plan/weekly-plan.helper.ts
+++ src/modules/weekly-plan/weekly-plan.helper.ts
@@ -19,7 +19,7 @@ export const isValidDay = (day: String) => {
 
 export const parseDate = (input: string) => {
   const [day, month, year] = input.split("-").map(Number);
-  return new Date(year, month - 1, day);
+  return new Date(Date.UTC(year, month - 1, day)).toISOString();
 };
 
 export const convertToISO = (dateStr: string) => {
